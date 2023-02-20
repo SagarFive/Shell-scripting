bdb31579f081c5cf8edae70e527ed2426953d510
SagarFive <73780805+SagarFive@users.noreply.github.com>
2023-02-20 18:07:22 +0530

    Update shell_while_loop.sh

--- Loops/shell_while_loop.sh
+++ Loops/shell_while_loop.sh
@@ -4,8 +4,12 @@
 #Author : sagarfive.in
 #Created on : 19-02-2022 on Amazon Linux 2 machin
 ################################
+#Reading starting number and assigning that number to variable a
 read -p "Enter a number to list serials up to 100 : "  a
+#
+#while loop from starting number to 101 interation means upto 100
 while [ $a -lt 101 ]
+#do print each number from starting number upto 100
 do
    echo $a
    a=`expr $a + 1`
