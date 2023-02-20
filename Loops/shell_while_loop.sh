660bee59d4c5d50f70576da408339bec2843f3c8
SagarFive <73780805+SagarFive@users.noreply.github.com>
2023-02-20 18:03:46 +0530

    Update shell_while_loop.sh

--- Loops/shell_while_loop.sh
+++ Loops/shell_while_loop.sh
@@ -2,7 +2,7 @@
 ################################
 #File : shell while loop
 #Author : sagarfive.in
-#Created on : 19-02-2022 on Amazon Linux 3 machin
+#Created on : 19-02-2022 on Amazon Linux 2 machin
 ################################
 read -p "Enter a number to list serials up to 100 : "  a
 while [ $a -lt 101 ]
